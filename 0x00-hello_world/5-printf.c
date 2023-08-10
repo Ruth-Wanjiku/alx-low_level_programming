53f22fff7902bcbadf9ee0bfb1cf5b1753875607
Ruth-Wanjiku <rwnganga25@gmail.com>
2023-08-10 08:13:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include<std.h> 
+#include<stdio.h>
 /**
- * main - A program that uses printf function 
+ * main - Entry point
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0)
+		return (0);
 }
